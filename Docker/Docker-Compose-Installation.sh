e4d189146a052b875cd1ce82e375f61b6cabfadc
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2019-07-17 16:47:23 +0530

    Update and rename Docker-Compose-Installation to Docker-Compose-Installation.sh

---
+++ Docker/Docker-Compose-Installation.sh
@@ -0,0 +1,10 @@
+Docker compose installation:
+-----------------------------------------
+
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+	sudo chmod +x /usr/local/bin/docker-compose
+
+	ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+	docker-compose --version
