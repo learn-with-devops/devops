5476425fae39af22733d0faba1bc90258b5088fb
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2020-01-22 15:28:25 +0530

    Create stop_ec2_instances.py

---
+++ Boto3/stop_ec2_instances.py
@@ -0,0 +1,31 @@
+## Stop All the Ece Instances by Irrespective of Tag.
+
+import boto3 
+
+#re = boto3.resource('ec2')
+#print(dir(re))
+print( "==================================")
+
+
+ec = boto3.client('ec2')
+
+#print(type(ec))
+
+#print(dir(ec))
+
+inst = ec.describe_instances()
+#print(inst)
+
+print( "==================================")
+
+instancelist = []
+for i in inst['Reservations']:
+  for st in i['Instances']:
+    print(st['InstanceId'])
+    instancelist.append(st['InstanceId'])
+print("-------------------")
+print(instancelist)
+ec.stop_instances(InstanceIds = instancelist)
+    
+    # print("Public IP Address : " + st['PublicIpAddress'], "Public DNS Name : " + st['PublicDnsName'], "Instance ID : " + st['InstanceId'])
+    # print(ec.stop_instances(InstanceIds = ['i-098d91a038f924470']))
