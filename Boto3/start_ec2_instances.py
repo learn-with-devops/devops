e28b839e5a57c5f75afdf9eff390237dcc951170
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2020-02-04 12:03:33 +0530

    Update start_ec2_instances.py

--- Boto3/start_ec2_instances.py
+++ Boto3/start_ec2_instances.py
@@ -1,4 +1,4 @@
-## Start Ec2 Instances by irrespective of Tags
+## Start all Ec2 Instances in a region
 
 import boto3 
 
@@ -28,3 +28,37 @@ ec.start_instances(InstanceIds = instancelist)
     
 # print("Public IP Address : " + st['PublicIpAddress'], "Public DNS Name : " + st['PublicDnsName'], "Instance ID : " + st['InstanceId'])
 # print(ec.stop_instances(InstanceIds = ['i-098d91a038f924470']))
+
+
+
+
+## Start EC2 instances based on tags
+
+import boto3
+import time
+
+ec = boto3.client('ec2')
+
+inst = ec.describe_instances(Filters=[{'Name': 'tag:name', 'Values': ['backup']}])
+#print(inst)
+
+instancelist = []
+for i in inst['Reservations']:
+  for st in i['Instances']:
+#    print(st['InstanceId'], st['PublicIpAddress'])
+    instancelist.append(st['InstanceId'])
+
+#print(instancelist)
+
+print("----------------- Ec2 Instance Status before Stopping-----------------")
+for ins in instancelist:
+	print(ins, st['State']['Name'])
+
+ec.stop_instances(InstanceIds = instancelist)
+
+# Sleep for 15 mins
+time.sleep(20)
+
+print("----------------- Ec2 Instance Status After Stopping-----------------")
+for ins1 in instancelist:
+	print(ins1, st['State']['Name'])
