71328a1ce279c37a3a8922cb01d6d23d609194cc
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2020-02-04 12:04:31 +0530

    Update stop_ec2_instances.py

--- Boto3/stop_ec2_instances.py
+++ Boto3/stop_ec2_instances.py
@@ -29,3 +29,35 @@ ec.stop_instances(InstanceIds = instancelist)
     
     # print("Public IP Address : " + st['PublicIpAddress'], "Public DNS Name : " + st['PublicDnsName'], "Instance ID : " + st['InstanceId'])
     # print(ec.stop_instances(InstanceIds = ['i-098d91a038f924470']))
+
+    
+## Stop Ec2 Instances based on Tags
+
+import boto3
+import time
+
+ec = boto3.client('ec2')
+
+inst = ec.describe_instances(Filters=[{'Name': 'tag:name', 'Values': ['backup']}])
+#print(inst)
+
+instancelist = []
+for i in inst['Reservations']:
+  for st in i['Instances']:
+#    print(st['InstanceId'], st['PublicIpAddress'])
+    instancelist.append(st['InstanceId'])
+
+#print(instancelist)
+
+print("----------------- Ec2 Instance Status before Stopping-----------------")
+for ins in instancelist:
+	print(ins, st['State']['Name'])
+
+ec.stop_instances(InstanceIds = instancelist)
+
+# Sleep for 15 mins
+time.sleep(20)
+
+print("----------------- Ec2 Instance Status After Stopping-----------------")
+for ins1 in instancelist:
+	print(ins1, st['State']['Name'])
