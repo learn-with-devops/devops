e582110ffea2ea8e2bd7117ee6b409c787316962
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2020-11-06 13:40:16 +0530

    Create app.py

---
+++ LoginForm/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, escape, request, render_template
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Anand@123',
+    database='gatewaytech'
+)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        mypass = request.form['pass']
+        re_pass = request.form['re_pass']
+        data = mydb.cursor()
+        data.execute("INSERT INTO gatewaylogin(name, email, pass, re_pass) values(%s, %s, SHA1(%s), SHA1(%s))",(name, email, mypass, re_pass))
+        mydb.commit()
+        data.close()
+        return "Data Inserted Successfully"
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='80')
