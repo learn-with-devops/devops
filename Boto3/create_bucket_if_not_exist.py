5a3e8760cd0926f99a7eab78dc297914ac342970
learn-cloud <githubanand@gmail.com>
2020-12-08 02:26:35 +0530

    s3 bucket

--- Boto3/create_bucket_if_not_exist.py
+++ Boto3/create_bucket_if_not_exist.py
@@ -62,6 +62,7 @@ create_bucket(sys.argv[1], sys.argv[2])
 # #     print(bucket['Name'])
 
 
+### Example 1
 # import logging
 # import boto3
 # from botocore.exceptions import ClientError
@@ -95,3 +96,30 @@ create_bucket(sys.argv[1], sys.argv[2])
 #
 #
 # create_bucket("anand-839-babu","ap-south-1")
+
+
+
+### Example 2
+# import boto3, botocore
+# s3 = boto3.resource('s3')
+# bucket_name = 'some-private-bucket'
+# #bucket_name = 'bucket-to-check'
+#
+# bucket = s3.Bucket(bucket_name)
+# def check_bucket(bucket):
+#     try:
+#         s3.meta.client.head_bucket(Bucket=bucket_name)
+#         print("Bucket Exists!")
+#         return True
+#     except botocore.exceptions.ClientError as e:
+#         # If a client error is thrown, then check that it was a 404 error.
+#         # If it was a 404 error, then the bucket does not exist.
+#         error_code = int(e.response['Error']['Code'])
+#         if error_code == 403:
+#             print("Private Bucket. Forbidden Access!")
+#             return True
+#         elif error_code == 404:
+#             print("Bucket Does Not Exist!")
+#             return False
+#
+# check_bucket(bucket)
