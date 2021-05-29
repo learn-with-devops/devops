18811f212ed5e6a9f3f8c429822c3928ad14c14a
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-05-29 12:00:19 +0530

    Update stop_public_ec2_instances_with_tags.py

--- Boto3/stop_public_ec2_instances_with_tags.py
+++ Boto3/stop_public_ec2_instances_with_tags.py
@@ -31,3 +31,57 @@ data.stop_instances(InstanceIds=public_instances_running)
 # print(instance_list)
 # # print(t)
 # print(public_instances_running)
+
+
+--------------------------------------------------------
+
+## Onother way 
+
+import json
+import boto3
+
+region = 'us-east-1'
+
+ec2 = boto3.client('ec2', region_name=region)
+
+def get_instance_ids(instance_names):
+
+    all_instances = ec2.describe_instances()
+    
+    instance_ids = []
+    
+    # find instance-id based on instance name
+    # many for loops but should work
+    for instance_name in instance_names:
+        for reservation in all_instances['Reservations']:
+            for instance in reservation['Instances']:
+                if 'Tags' in instance:
+                    for tag in instance['Tags']:
+                        if tag['Key'] == 'Name' \
+                            and tag['Value'] == instance_name:
+                            instance_ids.append(instance['InstanceId'])
+                            
+    return instance_ids
+
+def lambda_handler(event, context):
+    
+    instance_names = event["instances"].split(',')
+    action = event["action"]
+
+    instance_ids = get_instance_ids(instance_names)
+
+    print(instance_ids)
+
+    if action == 'Start':
+        print("STARTing your instances: " + str(instance_ids))
+        ec2.start_instances(InstanceIds=instance_ids)
+        response = "Successfully started instances: " + str(instance_ids)
+    elif action == 'Stop':
+        print("STOPping your instances: " + str(instance_ids))
+        ec2.stop_instances(InstanceIds=instance_ids)
+        response = "Successfully stopped instances: " + str(instance_ids)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
